44d6dc7bf6b7da77822abdb3c75378480300eb57
datadudeml <datadude.ml@gmail.com>
2025-04-18 07:35:02 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Vikram',
+    author_email= 'datadude.ml@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
